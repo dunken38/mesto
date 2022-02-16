9146c5fd7ac7b75ab0dd5d75c593a83efb2b1581
Dmitry Shibakin <dunken38@yandex.ru>
2022-02-16 14:08:28 +0600

    After 3rd review

--- scripts/index.js
+++ scripts/index.js
@@ -2,6 +2,7 @@
 const page = document.querySelector('.page');
 const editButton = document.querySelector('.profile__info-edit-button');
 const popup = page.querySelector('.popup');
+const popupActive = popup.querySelector('.popup_active');
 const popupEditForm = popup.querySelector('[name=popup-edit-form]'); //отбор всех form сделал по именам чтобы удалить лишние классы
 const cancelButton = page.querySelector('.popup__cancel-button');
 const profileInfoName = page.querySelector('.profile__info-name');
@@ -9,16 +10,16 @@ const profileInfoAbout = page.querySelector('.profile__info-about');
 const inputName = popupEditForm.querySelector('[name=input-name]'); 
 const inputAbout = popupEditForm.querySelector('[name=input-about]');
 
-//тут переделал: вместо добавления классов со стилями display просто меняю стили в одном классе
+//открываем окошко
 const popupOpen = () => {
-  popup.style.display = 'flex'; //изначально в селекторе класса popup display:none
+  popup.classList.add('popup_active');
   inputName.value = profileInfoName.textContent; //получаем данные в форму из информации со страницы
   inputAbout.value = profileInfoAbout.textContent;
 }
 
-//тут возвращаем стиль display='none' для сокрытия окошка popup
+//закрываем окошко
 const popupClose = () => {
-  popup.style.display = 'none';
+  popup.classList.remove('popup_active');
 }
 
 //и дальше кнопку save с заменой полей из popup в profile
