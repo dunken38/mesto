5f67012ad1b37593a76fd4713b75dc972e7576ed
Dmitry Shibakin <dunken38@yandex.ru>
2022-02-20 23:08:57 +0600

    20220220

--- scripts/index.js
+++ scripts/index.js
@@ -13,60 +13,16 @@ const inputName = popupEditForm.querySelector('[name=input-name]');
 const inputAbout = popupEditForm.querySelector('[name=input-about]');
 const saveButton = popup.querySelector('.popup__save-button'); 
 const popupWindow = document.querySelector('.popup-window');
-const likeState = document.querySelectorAll('.element__like'); //для функционала по лайкам
 popupWindow.append(popupTemplate); //добавляем то что получилось в popup в зависимости от нажатой кнопки (edit or add)
   //теперь блок для gallery (карточек)
-const gallery = document.querySelector('#gallery').content.cloneNode(true); //забираем template для карточек
-
-//теперь пишем функции
-//закрываем окошко popup
-const popupClose = () => {
-  popup.classList.remove('popup_active');
-}
-
-//кнопка save с заменой полей из popup в profile
-const getValueOfInput = (evt) => { 
-  evt.preventDefault();
-  profileInfoName.textContent = inputName.value;
-  profileInfoAbout.textContent = inputAbout.value;
-  popupClose(); //тут закрываем форму без отправки на сервер
-}
-
-//вставляем карточку
-const addElement = (evt) => { 
-  evt.preventDefault();
-  popupClose(); //тут закрываем форму без отправки на сервер
-}
-
-//что происходит при нажатии на кнопку Edit
-const popupOpenOnEditButton = () => {
-  popupTitle.textContent = 'Редактировать профиль';
-  saveButton.textContent = 'Сохранить';
-  inputName.value = profileInfoName.textContent; //получаем данные в форму из информации со страницы
-  inputAbout.value = profileInfoAbout.textContent;
-  popup.classList.add('popup_active');
-  popupEditForm.addEventListener('submit',getValueOfInput,false); //закрываем окошко popup по клику на Сохранить.False добавлен для того чтобы форма не обновлялась.
-}
-
-//что происходит при нажатии на кнопку Add
-const popupOpenOnAddButton = () => {
-  popupEditForm.removeEventListener('submit',getValueOfInput,false); //удаляем слушатель из popupOpenOnEditButton
-  inputName.value = '';
-  inputAbout.value = '';
-  popupTitle.textContent = 'Новое место';
-  saveButton.textContent = 'Создать';
-  inputName.placeholder = "Название";
-  inputAbout.placeholder = "Ссылка на картинку";
-  popup.classList.add('popup_active');
-  popupEditForm.addEventListener('submit',addElement,false);
-}
-
-//слушатели кнопок
-editButton.addEventListener('click',popupOpenOnEditButton); //открываем окошко popup по клику на edit
-addButton.addEventListener('click',popupOpenOnAddButton); //открываем окошко popup по клику на add
-cancelButton.addEventListener('click',popupClose); //закрываем окошко popup по клику на крестик
-
-/*
+const galleryTemplate = document.querySelector('#gallery').content.cloneNode(true); //забираем template для карточек
+const galleryImage = galleryTemplate.querySelector('.element__image');
+const galleryText = galleryTemplate.querySelector('.element__text');
+const galleryElements = document.querySelector('.elements');
+const emptyCards = document.querySelector('.element__empty-cards'); //чтобы убирать надпись "Добавьте карточку"
+const galleryTrashButton = galleryTemplate.querySelectorAll('.element__trash'); //для функционала по корзине
+const likeState = galleryTemplate.querySelectorAll('.element__like'); //для функционала по лайкам
+//объект с элементами карточек
 const initialCards = [
   {
     name: 'Архыз',
@@ -92,10 +48,70 @@ const initialCards = [
     name: 'Байкал',
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
   }
-]; 
-*/
+];
 
-//лайки
+//теперь пишем функции
+//закрываем окошко popup
+const popupClose = () => {
+  popup.classList.remove('popup_active');
+}
+
+//кнопка save с заменой полей из popup в profile
+const getValueOfInput = (evt) => { 
+  evt.preventDefault();
+  profileInfoName.textContent = inputName.value;
+  profileInfoAbout.textContent = inputAbout.value;
+  popupClose(); //тут закрываем форму без отправки на сервер
+}
+
+//вставляем карточку
+const addElement = (evt) => { 
+  evt.preventDefault();
+  emptyCards.remove();
+  galleryText.textContent = inputName.value;
+  galleryImage.src = inputAbout.value;
+  galleryElements.append(galleryTemplate);
+  popupClose(); //тут закрываем форму без отправки на сервер
+  console.log(galleryElements);
+}
+
+//удаляем карточку
+const deleteElement = () => {
+  const closeDelElement = galleryTrashButton.closest('.element');
+  console.log(closeDelElement);
+  closeDelElement.remove();
+  if (galleryElements.children.length == 0) {
+    galleryElements.append(emptyCards);
+  }
+  console.log(galleryElements);
+}
+
+//что происходит при нажатии на кнопку Edit
+const popupOpenOnEditButton = () => {
+  popupTitle.textContent = 'Редактировать профиль';
+  saveButton.textContent = 'Сохранить';
+  inputName.placeholder = "Введите ваше имя";
+  inputAbout.placeholder = "Напишите Ваши увлечения";
+  inputName.value = profileInfoName.textContent; //получаем данные в форму из информации со страницы
+  inputAbout.value = profileInfoAbout.textContent;
+  popup.classList.add('popup_active');
+  popupEditForm.addEventListener('submit',getValueOfInput,false); //закрываем окошко popup по клику на Сохранить.False добавлен для того чтобы форма не обновлялась.
+}
+
+//что происходит при нажатии на кнопку Add
+const popupOpenOnAddButton = () => {
+  popupEditForm.removeEventListener('submit',getValueOfInput,false); //удаляем слушатель из popupOpenOnEditButton
+  inputName.value = '';
+  inputAbout.value = '';
+  popupTitle.textContent = 'Новое место';
+  saveButton.textContent = 'Создать';
+  inputName.placeholder = "Название";
+  inputAbout.placeholder = "Ссылка на картинку";
+  popup.classList.add('popup_active');
+  popupEditForm.addEventListener('submit',addElement,false);
+}
+
+//лайки со слушателем
 for (let i = 0; i < likeState.length; i++){
   likeState[i].addEventListener('click', () => {  
     if (!likeState[i].classList.contains('element__like_active')) {
@@ -106,3 +122,9 @@ for (let i = 0; i < likeState.length; i++){
     } 
   });
 };
+
+//слушатели кнопок
+editButton.addEventListener('click',popupOpenOnEditButton); //открываем окошко popup по клику на edit
+addButton.addEventListener('click',popupOpenOnAddButton); //открываем окошко popup по клику на add
+cancelButton.addEventListener('click',popupClose); //закрываем окошко popup по клику на крестик
+
