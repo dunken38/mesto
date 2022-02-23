b6f88ced290dcda4049b48057f3e277bc67f4602
Dmitry Shibakin <dunken38@yandex.ru>
2022-02-23 23:10:46 +0600

    №5 Ready

--- scripts/index.js
+++ scripts/index.js
@@ -1,32 +1,34 @@
 //объявляем переменные
-  //сначала блок для popup
-const popupTemplate = document.querySelector('#popupTemplate').content.cloneNode(true); //забираем template для двух окошек edit и add
-const popup = popupTemplate.querySelector('.popup');
+const popupTemplate = document.querySelector('#popupTemplate').content; //забираем template для двух окошек edit и add
+const popup = popupTemplate.querySelector('.popup').cloneNode(true);
+const galleryElements = document.querySelector('.elements');
+const emptyCards = document.querySelector('.element__empty-cards'); //чтобы убирать надпись "Добавьте карточку"
+const galleryTemplate = document.querySelector('#gallery').content; //забираем template для карточек
+const popupWindow = document.querySelector('.popup-window');
+const profileInfoName = document.querySelector('.profile__info-name');
+const profileInfoAbout = document.querySelector('.profile__info-about');
 const editButton = document.querySelector('.profile__info-edit-button');
 const addButton = document.querySelector('.profile__add-button');
 const popupEditForm = popup.querySelector('[name=popup-edit-form]'); //отбор всех form сделал по именам чтобы удалить лишние классы
 const cancelButton = popup.querySelector('.popup__cancel-button');
 const popupTitle = popup.querySelector('.popup__title');
-
-const popupImage = popup.querySelector('.popup__image');
-const profileInfoName = document.querySelector('.profile__info-name');
-const profileInfoAbout = document.querySelector('.profile__info-about');
+const popupContentDocument = popup.querySelector('.popup__content');
+const saveButton = popup.querySelector('.popup__save-button'); 
+const popupContent = popup.querySelector('.popup__content');
 const inputName = popupEditForm.querySelector('[name=input-name]'); 
 const inputAbout = popupEditForm.querySelector('[name=input-about]');
-const saveButton = popup.querySelector('.popup__save-button'); 
-const popupWindow = document.querySelector('.popup-window');
-popupWindow.append(popupTemplate); //добавляем то что получилось в popup в зависимости от нажатой кнопки (edit or add)
-  //теперь блок для gallery (карточек)
-const galleryElements = document.querySelector('.elements');
-const emptyCards = document.querySelector('.element__empty-cards'); //чтобы убирать надпись "Добавьте карточку"
-const galleryTemplate = document.querySelector('#gallery').content; //забираем template для карточек
 const galleryImage = galleryTemplate.querySelector('.element__image');
 const galleryText = galleryTemplate.querySelector('.element__text');
-const popupContentDocument = document.querySelector('.popup__content');
+const imageTemplate = document.querySelector('#imageTemplate').content; //забираем из template для большой картинки при клике
+const imagePopup = imageTemplate.querySelector('.popup').cloneNode(true);
+const imagePopupContent = imagePopup.querySelector('.popup__image-content');
+const popupImage = imagePopup.querySelector('.popup__image');
+const popupImageText = imagePopup.querySelector('.popup__image-text');
+const imageCancelButton = imagePopup.querySelector('.popup__cancel-button');
+popupWindow.append(popup); //добавляем то что получилось в popup в зависимости от нажатой кнопки (edit or add)
+popupWindow.append(imagePopup); //добавляем при увеличении картинки
 
-
-
-//объект с элементами карточек
+//массив с элементами карточек
 const initialCards = [
   {
     name: 'Архыз',
@@ -55,10 +57,14 @@ const initialCards = [
 ];
 
 //теперь пишем функции
-//закрываем окошко popup
+//закрываем окошко popup и imagePopup
 const popupClose = () => {
   popup.classList.remove('popup_active');
 }
+const imagePopupClose = () => {
+  imagePopup.classList.remove('popup_active');
+}
+
 //кнопка save с заменой полей из popup в profile
 const getValueOfInput = (evt) => { 
   evt.preventDefault();
@@ -67,11 +73,8 @@ const getValueOfInput = (evt) => {
   popupClose(); //тут закрываем форму без отправки на сервер
 }
 
-//что происходит при нажатии на картинку
-
-
 //вставляем карточку
-const addElement = (name,link) =>{ 
+const addElement = (name,link) => { 
   galleryText.textContent = name;
   galleryImage.src = link;
   const galleryElement = galleryTemplate.querySelector('.element').cloneNode(true);
@@ -92,45 +95,46 @@ const addElement = (name,link) =>{
     }
   if (emptyCards) { //тут проверяем если emptyCards присутствует,то удаляем его при добавлении карточки
     emptyCards.remove();
-  }  
-  //открытие картинки по нажатию
-  const imageFromGallery = document.querySelectorAll('.element__image');
-  const popupContent = document.querySelector('.popup__content');
-  for (i = 0; i < imageFromGallery.length; i++) {
-    imageFromGallery[i].addEventListener('click', (event) => {
-      console.log(event.target); 
-      popupEditForm.removeEventListener('submit',getValueOfInput,false); //удаляем слушатель из popupOpenOnEditButton
-      popupEditForm.removeEventListener('submit', addElementAndPreventDefault, false);
-      saveButton.remove();
-      popupEditForm.remove();
-      popupImage.src = event.target.src;
-      popup.classList.add('popup_active'); 
-    });
   }
-  popupClose();
+  //открытие картинки по нажатию
+  const imageFromGallery = document.querySelectorAll('.element__image');  
+  const bigImage = (event) => {
+    popupEditForm.removeEventListener('submit',getValueOfInput,false); //удаляем слушатель из popupOpenOnEditButton
+    popupEditForm.removeEventListener('submit', addElementAndPreventDefault, false);
+    imagePopupContent.style.backgroundColor = 'transparent';
+    popupImage.src = event.target.src;
+    popupImageText.textContent = event.target.nextElementSibling.firstElementChild.textContent;
+    imagePopup.classList.add('popup_active'); 
+    
+  }
+  for (i = 0; i < imageFromGallery.length; i++) {
+    imageFromGallery[i].addEventListener('click',bigImage); 
+  }
 }
 
-for (let i = 0; i < initialCards.length; i++){ //тут карточки из готового массива выше
+//тут 6 карточек из готового массива выше
+for (let i = 0; i < initialCards.length; i++){ 
   addElement(initialCards[i].name,initialCards[i].link);
 }
 
 //что происходит при нажатии на кнопку Edit
 const popupOpenOnEditButton = () => {
-  document.removeEventListener('submit', addElementAndPreventDefault, false);
-  //document.removeEventListener('click',bigImage);
+  popupEditForm.removeEventListener('submit', addElementAndPreventDefault, false);
   popupTitle.textContent = "Редактировать профиль";
   saveButton.textContent = "Сохранить";
   inputName.placeholder = "Введите ваше имя";
   inputAbout.placeholder = "Напишите Ваши увлечения";
   inputName.value = profileInfoName.textContent; //получаем данные в форму из информации со страницы
   inputAbout.value = profileInfoAbout.textContent;
-  popup.classList.add('popup_active');
+  popup.classList.add('popup_active');  
   popupEditForm.addEventListener('submit',getValueOfInput,false); //закрываем окошко popup по клику на Сохранить.False добавлен для того чтобы форма не обновлялась.
 }
 
-const addElementAndPreventDefault = (evt) => { //тут вынесли функцию со слушателя submit в окошке add чтобы корректно пользоваться добавлением и отменой слушателя
+//тут вынесли функцию со слушателя submit в окошке add чтобы корректно пользоваться добавлением и отменой слушателя
+const addElementAndPreventDefault = (evt) => { 
   evt.preventDefault();
   addElement(inputName.value,inputAbout.value);
+  popupClose();
 }
 
 //что происходит при нажатии на кнопку Add
@@ -143,17 +147,11 @@ const popupOpenOnAddButton = () => {
   inputName.placeholder = "Название";
   inputAbout.placeholder = "Ссылка на картинку";
   popup.classList.add('popup_active');
-  popupEditForm.addEventListener('submit', addElementAndPreventDefault, false);
+  popupEditForm.addEventListener('submit',addElementAndPreventDefault,false);
 }
-
-
 
 //слушатели кнопок
 editButton.addEventListener('click',popupOpenOnEditButton); //открываем окошко popup по клику на edit
 addButton.addEventListener('click',popupOpenOnAddButton); //открываем окошко popup по клику на add
 cancelButton.addEventListener('click',popupClose); //закрываем окошко popup по клику на крестик
-
-
-
-
-
+imageCancelButton.addEventListener('click',imagePopupClose); //закрываем большую картинку
